858e76e884dc571869c5e5b4a97c9826197e75cf
plantainbread <zyuanboost2026@gmail.com>
2026-03-04 01:27:52 -0800

    create constants file to make editing values easier

---
+++ constants.py
@@ -0,0 +1,12 @@
+from typing import Final
+
+"""
+Constants file containing information such as motor controller IDs,
+wheel radius, and more for editing convenience.
+"""
+
+class DriveConstants:
+    # None as placeholder
+    DRIVE_CONTROLLER_ID: Final[int] = None
+    DRIVE_WHEEL_RADIUS: Final[float] = None
+    
