c305bf4356b6f1cb28490b532bd96fa60e27a8ff
Ziyao Jasmine Declan <ehsjetnet@gmail.com>
2026-03-31 00:20:12 +0100

    change constants

--- mainbuild.py
+++ mainbuild.py
@@ -81,10 +81,10 @@ def _HELPER_import_constants():
         DRIVE_CONTROLLER_ID: Final[str] = "6_17986660329004823992" # Need to check in Dawn
         DRIVE_WHEEL_RADIUS: Final[float] = 0.101 # Need to measure, convert from inches to meters.
         DRIVE_MOTOR_TICKS_PER_ROTATION: Final[int] = 16 # Assuming same motor probably the same
-        DRIVE_MOTOR_RATIO: Final[int] = 70 # Need to verify
+        DRIVE_MOTOR_RATIO: Final[int] = 50 # Need to verify
         DRIVE_WHEEL_SPAN: Final[float] = 0.152 # May need to remeasure
         # original for above: 0.258 meters
-        HUB_TO_WHEEL_GEAR_RATIO: Final[float] = 84 / 36 # gear further from motor / gear closer to motor (Remeasure)
+        HUB_TO_WHEEL_GEAR_RATIO: Final[float] = 36 / 36 # gear further from motor / gear closer to motor (Remeasure)
         KEYBOARD_DRIVE_SPEED: Final[float] = 0.8 # placeholder, may change after testing
         KEYBOARD_TURN_SPEED: Final[float] = 0.8 # placeholder, may change after testing
     
